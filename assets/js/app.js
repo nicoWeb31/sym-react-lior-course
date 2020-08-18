8ffe086732948a76df3419ae7133b65bef513145
nicoWeb31 <nico.riot@free.fr>
2020-08-18 21:00:17 +0200

    mise en place de react

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,23 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Navbar from './components/Navbar'
+import Homepage from './pages/Homepage';
 
-// any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+const App = () =>{
+    return (
+
+    <>
+        <Navbar/>
+        <div className="container pt-5">
+            <Homepage/>
+        </div>
+    </>
+
+    )
+}
+
+const rootElement = document.querySelector('#app');
+ReactDOM.render(<App/>,rootElement);
