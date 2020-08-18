1775c4fa87d2285ea42c8cafaa409c2af6a82310
nicoWeb31 <nico.riot@free.fr>
2020-08-18 19:18:22 +0200

    debug query intercept

--- src/doctrine/CurrentUserExtension.php
+++ src/doctrine/CurrentUserExtension.php
@@ -10,48 +10,51 @@ use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
 use App\Entity\Customer;
 use App\Entity\Invoice;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class CurrentUserExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
 
 {
 
     private $security;
+    private $auth;
 
-    public function __construct(Security $security)
+    public function __construct(Security $security,AuthorizationCheckerInterface $cheker)
     {
         $this->security = $security;
+        $this->auth = $cheker;
     }
 
 
     private function addWhere(QueryBuilder $queryBuilder, string $resourceClass){
 
         //obtenir l'utilisateur connecté
-        $user =$this->security->getUser();
-
+        
+        
+        
         //si on demande des invoice ou des customer tenir compte de l'utilisateur connecté
-        if($resourceClass === Customer::class || $resourceClass === Invoice::class){
-            $rootAlias = $queryBuilder->getRootAliases()[0];
-
-
-            if($resourceClass === Customer::class){
+        if(($resourceClass === Customer::class || $resourceClass === Invoice::class) && !$this->auth->isGranted('ROLE_ADMIN')){
             
-                $queryBuilder->andWhere("$rootAlias.user = :user");
+                $user =$this->security->getUser();
+                $rootAlias = $queryBuilder->getRootAliases()[0];
+    
+                if($resourceClass === Customer::class){
+                
+                    $queryBuilder->andWhere("$rootAlias.user = :user");
+                
+    
+    
+                }elseif($resourceClass === Invoice::class ){
+                    $queryBuilder->join("$rootAlias.customer","c")
+                                ->andWhere("c.user= :user");
+                                
+    
+    
+                }
+                $queryBuilder->setParameter(":user",$user);
             
-
-
-            }elseif($resourceClass === Invoice::class){
-                $queryBuilder->join("$rootAlias.customer","c")
-                            ->andWhere("c.user= :user");
-                            
-
-
-            }
-            $queryBuilder->setParameter(":user",$user);
-
-
-
         }
-
+    
     }
 
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, ?string $operationName = null)
