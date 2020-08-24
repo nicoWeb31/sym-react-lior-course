e9c1e3d29735408cd41e50399f4ae87f9ff65490
nicoWeb31 <nico.riot@free.fr>
2020-08-24 18:14:53 +0200

    formulaire de connection

--- assets/js/pages/LoginPage.jsx
+++ assets/js/pages/LoginPage.jsx
@@ -1,26 +1,46 @@
 import React, { useState } from 'react';
+import axios from "axios";
 
 const LoginPage = () => {
 
     const [credentials, setCredentials] = useState({
-        username : "",
-        password : ""
+        username: "",
+        password: ""
     })
+
+    const [error, seterror] = useState("");
 
 
     //changement du formulaire
-    const handleChange = (e) =>{
+    const handleChange = (e) => {
         const value = e.currentTarget.value;
-        const name = e.currentTarget.name; 
-        setCredentials({...credentials,[name]:value})
+        const name = e.currentTarget.name;
+        setCredentials({ ...credentials, [name]: value })
     }
 
     //soumission du formulaire
 
-    const handleSubmit = e =>{
+    const handleSubmit = async e => {
         e.preventDefault();
+        //console.log(credentials)
+        try {
+            const token = await axios.post('https://127.0.0.1:8000/api/login_check', credentials)
+                .then(resp => resp.data.token);
+            seterror("");
 
-        console.log(credentials)
+            //je stock le token dans le storage
+            window.localStorage.setItem("authToken",token);
+                
+            //on demande a axios d'utiliser le token ds les prochaines requetes dans son header
+            axios.defaults.headers["authorization"] = `Bearer ${token}` 
+
+        }
+        catch (err) {
+            console.log(err.response)
+            seterror("les indentifiant ne corresponde pas")
+
+        }
+
     }
 
     return (
@@ -31,7 +51,7 @@ const LoginPage = () => {
                 <div className="form-group">
                     <label htmlFor="username">Adresse email</label>
                     <input type="email"
-                        className="form-control"
+                        className={"form-control" + (error && " is-invalid")}
                         placeholder="adresse email de connection"
                         name="username"
                         id="username"
@@ -39,6 +59,10 @@ const LoginPage = () => {
                         onChange={handleChange}
 
                     />
+
+                    {error &&
+                        <p className="invalid-feedback">{error}</p>
+                    }
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
@@ -54,7 +78,7 @@ const LoginPage = () => {
                 </div>
                 <div className="form-group">
                     <button type="submit" className="btn btn-success">
-                    je me connect
+                        je me connect
                     </button>
                 </div>
             </form>
